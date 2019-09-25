8d4a2ee7967dcc7af8f6284090bffbc321a31530
Devin Warrick <dev.warr@gmail.com>
2019-09-24 20:39:49 -0500

    Push folder and not submodule to Github

---
+++ tweet-test/src/utils/data.js
@@ -0,0 +1,78 @@
+export const template = {
+  programming_language: "I was programming in ",
+  noun: ", trying to get all of my ",
+  verb: " to properly ",
+  ing_verb: ". However, nothing was actually ",
+  ed_verb: "...\n\nIt was then I realized I hadn't even ",
+  noun2: " my ",
+  end: "."
+};
+
+export const tinyDict = {
+  programming_language: [
+    "Javascript",
+    "React",
+    "React Native",
+    "COBOL",
+    "Python",
+    "Kotlin",
+    "JS",
+    "Java",
+    "Swift",
+    "Mapbox"
+  ],
+  noun: [
+    "screen",
+    "backend",
+    "alligator",
+    "dog",
+    "man",
+    "developer",
+    "cat",
+    "x-code"
+  ],
+  verb: [
+    "debug",
+    "pwn",
+    "eat",
+    "sleep",
+    "run",
+    "eat",
+    "fight",
+    "code",
+    "jump",
+    "annoy"
+  ],
+  ing_verb: [
+    "debugging",
+    "falling",
+    "playing",
+    "running",
+    "jumping",
+    "breaking",
+    "typing"
+  ],
+  ed_verb: [
+    "typed",
+    "hacked",
+    "railed",
+    "waited",
+    "fled",
+    "cooked",
+    "loved",
+    "debugged",
+    "worked"
+  ],
+  noun2: [
+    "team",
+    "keyboard",
+    "sun",
+    "husband",
+    "Dev",
+    "School",
+    "branch",
+    "team",
+    "home",
+    "macBook"
+  ]
+};
